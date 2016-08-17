ac3fba4c9f2b61106e4f58d8b5d9efc0f89c804b
Devonte M <devontem@live.com>
2016-08-17 11:00:19 -0400

    updated user back-end API

--- server/users/routes.js
+++ server/users/routes.js
@@ -1,5 +1,7 @@
 var usersController = require('./usersController');
 
 module.exports = function(app){
+	app.post('/', usersController.createUser);
+	app.post('/:id', usersController.updateUser);
 	app.get('/:id', usersController.getUser);
 }
--- server/users/usersController.js
+++ server/users/usersController.js
@@ -1,3 +1,12 @@
+var User = require('./../db/models/User');
+
+module.exports.createUser = function(req, res){
+	res.send('getUser');
+}
+
+module.exports.updateUser = function(req, res){
+	res.send('getUser');
+}
 
 module.exports.getUser = function(req, res){
 	res.send('getUser');
