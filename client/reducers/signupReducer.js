71169ca94c2312badaea8a6d593b9bc963383f5d
Devonte M <devontem@live.com>
2016-10-03 11:39:02 -0400

    updated logic with signing up a user

---
+++ client/reducers/signupReducer.js
@@ -0,0 +1,19 @@
+// creating the signup reducer
+
+const signupReducer = function(state = {}, action){
+
+	switch (action.type) {
+		case "SIGNUP":
+			return state
+		case "SIGNUP_PENDING":
+			return {...state, pending: true}
+		case "SIGNUP_FULFILLED":
+			return {...state, success: true, pending: false, ...action.payload.data}
+		case "SIGNUP_REJECTED":
+			return {...state, error: true, pending: false, message: "There was an unexpected error on your file conversion. Please try again later."}
+		default:
+			return state
+	}
+}
+
+export default signupReducer;
