26cb19fb7c92ca4920a4aaefd54299992bfad79b
Devonte MacGlashan <devontem@yahoo-inc.com>
2017-10-30 16:32:06 -0700

    removed redundant comments

--- server/download/routes.js
+++ server/download/routes.js
@@ -2,7 +2,6 @@ var downloadController = require('./downloadController');
 
 module.exports = function(app){
 	app.post('/', downloadController.queryDownload);	
-	// app.get('/id/:id', downloadController.getDownload); // deprecated
 	app.post('/id/:id', downloadController.getDownload);
 	app.get('/check/id/:id', downloadController.checkDownload);
 }
--- server/users/routes.js
+++ server/users/routes.js
@@ -3,7 +3,6 @@ var checkAuth = require('./../helpers/authHelper');
 
 module.exports = function(app){
 	app.post('/', usersController.createUser);
-	// app.get('/:id', function(req, res){ res.send('hey')});
 	app.get('/:id', checkAuth, usersController.getUser);
 	app.put('/:id', checkAuth, usersController.updateUser);
 	app.get('/:id/feed', checkAuth, usersController.getFeed);
